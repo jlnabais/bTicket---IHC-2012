9cdbdde8fff9760fea5fa694fb58e9bc134ea3ff
João Nabais <jlnabais@gmail.com>
2012-04-01 16:01:54 +0100

    settings.py with postgresql_psycopg2

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 
 if 'DATABASE_URL' in os.environ:	
 	url = urlparse.urlparse(os.environ['DATABASE_URL'])
-	DATABASE_ENGINE = 'postgresql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+	DATABASE_ENGINE = 'postgresql_psycopg2'          # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 	DATABASE_NAME = url.path[1:]             # Or path to database file if using sqlite3.
 	DATABASE_USER = url.username             # Not used with sqlite3.
 	DATABASE_PASSWORD = url.password         # Not used with sqlite3.

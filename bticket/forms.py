ac23ee703819d732e09611563ab9bdf6d903e0ce
macedo <macedo@macedo-VirtualBox.(none)>
2012-05-25 12:36:05 +0100

    correccao de erros de ingles, e alteracao do display dos erros do register

--- bticket/forms.py
+++ bticket/forms.py
@@ -26,29 +26,29 @@ class CheckNuberOfTripsForm(forms.Form):
 
 class UserProfileRegistrationForm(ModelForm):
 	first_name_error = {
-		'required': mark_safe('<p style="color: red;">This field is required.</p>')
+		'required': mark_safe('<div class="alert alert-error" style="max-width:145px">First name is required.</div>')
 	}
 
 	first_name = forms.CharField(label = u'First Name', max_length = 30, error_messages = first_name_error)
 	
 	last_name_error = {
-		'required': mark_safe('<p style="color: red;">This field is required.</p>')
+		'required': mark_safe('<div class="alert alert-error" style="max-width:145px">Last name is required.</div>')
 	}
 	last_name = forms.CharField(label = u'Last Name', max_length = 30, error_messages = last_name_error)
 	
 	email_errors = {
-		'required': mark_safe('<p style="color: red;">This field is required.</p>'),
-		'invalid': mark_safe('<p style="color: red;">This field is invalid. Your email must follow the pattern: name@domain.com.</p>')
+		'required': mark_safe('<div class="alert alert-error" style="max-width:145px">Email is required.</div>'),
+		'invalid': mark_safe('<div class="alert alert-error" style="max-width:300px">Invalid email.<br>It must follow the pattern: name@domain.com.</div>')
 	}
 	email = forms.EmailField(label = u'Email', error_messages = email_errors)
 
 	username_error = {
-		'required': mark_safe('<p style="color: red;">This field is required.</p>')
+		'required': mark_safe('<div class="alert alert-error" style="max-width:145px">Username is required.</div>')
 	}
 	username = forms.CharField(label = u'Username', max_length = 30, error_messages = username_error)
 	
 	pass1_error = {
-		'required': mark_safe('<p style="color: red;">This field is required.</p>')
+		'required': mark_safe('<div class="alert alert-error" style="max-width:145px">Password is required.</div>')
 	}
 	password1 = forms.CharField(
 		label = u'Password',
@@ -56,7 +56,7 @@ class UserProfileRegistrationForm(ModelForm):
 		error_messages = pass1_error
 	)
 	pass2_error = {
-		'required': mark_safe('<p style="color: red;">This field is required.</p>')
+		'required': mark_safe('<div class="alert alert-error" style="max-width:175px">Re-type password is required.</div>')
 	}
 	password2 = forms.CharField(
 		label = u'Password(Again)',
@@ -70,7 +70,7 @@ class UserProfileRegistrationForm(ModelForm):
 		field_args = {
 			"avatar" : {
 				"error_messages" : {
-					'required': mark_safe('<p style="color: red;">This field is required.</p>')
+					'required': mark_safe('<div class="alert alert-error" style="max-width:145px">Avataard is required.</div>')
 				}
 			}
 		}
@@ -81,24 +81,24 @@ class UserProfileRegistrationForm(ModelForm):
 			password2 = self.cleaned_data['password2']
 			if password1 == password2:
 				return password2
-		raise forms.ValidationError(mark_safe('<p style="color: red;">Passwords do not match.</p>'))
+		raise forms.ValidationError(mark_safe('<div class="alert alert-error" style="max-width:145px">Passwords do not match.</div>'))
 		
 	def clean_username(self):
 		username = self.cleaned_data['username']
 		if not re.search(r'^\w+$', username):
-			raise forms.ValidationError(mark_safe('<p style="color: red;">Username can only contain '
-				'alphanumeric characters and the underscore.</p>'))
+			raise forms.ValidationError(mark_safe('<div class="alert alert-error" style="max-width:145px">Username can only contain '
+				'alphanumeric characters and the underscore.</div>'))
 		try:
 			User.objects.get(username = username)
 		except:
 			return username
-		raise forms.ValidationError(mark_safe('<p style="color: red;">Username is already taken.</p>'))
+		raise forms.ValidationError(mark_safe('<div class="alert alert-error" style="max-width:145px">Username is already taken.</div>'))
 
 	def clean_first_name(self):
 		first_name = self.cleaned_data['first_name']
 		if not re.search(r'(?iL)^[\s\*\?a-z]*$', first_name):
-			raise forms.ValidationError(mark_safe('<p style="color: red;">First name can only contain '
-				'a-z letters and whitespaces.</p>'))
+			raise forms.ValidationError(mark_safe('<div class="alert alert-error" style="max-width:145px">First name can only contain '
+				'a-z letters and whitespaces.</div>'))
 		try:
 			User.objects.get(first_name = first_name)
 		except:
@@ -107,13 +107,13 @@ class UserProfileRegistrationForm(ModelForm):
 	def clean_last_name(self):
 		last_name = self.cleaned_data['last_name']
 		if not re.search(r'(?iL)^[\s\*\?a-z]*$', last_name):
-			raise forms.ValidationError(mark_safe('<p style="color: red;">Last name can only contain '
-				'a-z letters and whitespaces.</p>'))
+			raise forms.ValidationError(mark_safe('<div class="alert alert-error" style="max-width:145px">Last name can only contain '
+				'a-z letters and whitespaces.</div>'))
 		try:
 			User.objects.get(last_name = last_name)
 		except:
 			return last_name
-			
+
 class UserProfileManagementForm(ModelForm):
 	class Meta:
 		model = UserProfile
